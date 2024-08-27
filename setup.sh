23349598da7d4a1a96deae7d73bf342c6eaff13a
nick fan <nickfan81@gmail.com>
2024-08-27 13:13:49 +0800

    fix: docker version update.
    Signed-off-by: nick fan <nickfan81@gmail.com>

--- setup.sh
+++ setup.sh
@@ -615,7 +615,7 @@ update_package_source(){
   if [[ ${INSTALL_PKG_ENABLE_DOCKER} -eq 1 ]];then
     for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
     install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
     chmod a+r /etc/apt/keyrings/docker.gpg
     if [[ ${SERVER_REGION_CN} == "y" ]];then
       echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
